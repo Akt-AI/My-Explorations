f38b1ba4a27b42a4e737d59c93bb37a660f24f8e
“Arun” <tiwaryarun084@gmail.com>
2022-06-13 22:08:35 +0530

    All files uploaded

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 #git add <folder1> <folder2> <etc.>
 git add  .
 git commit -m "All files uploaded"
-git remote add origin https://github.com/Akt-AI/All-Projects.git
+#git remote add origin https://github.com/Akt-AI/All-Projects.git
 git push -u origin master
 git push origin master  
 
