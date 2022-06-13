59c7f84bb9980503e82264ff3be20ced5c83b8ed
Akt-AI <tiwaryarun084@gmail.com>
2022-06-13 22:25:33 +0530

    Delete upload.sh

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#git init
-#git add <folder1> <folder2> <etc.>
-git add  .
-git commit -m "All files added"
-#git remote add origin https://github.com/Akt-AI/All-Projects.git
-#git push -u origin master
-#git push origin master 
-git push 
-
